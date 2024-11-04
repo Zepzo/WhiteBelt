9bd930252097161e5a018c7c2c559537aa4a679d
Zätha Malmberg <zathamalmberg@gmail.com>
2024-11-04 11:37:33 +0100

    MiniKata1 complete

---
+++ MiniKata1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniKata1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Zätha";
+        int health = 100;
+        float attackPower = 15.5f;
+        bool isParalised = true;
+        double coin = 24.75;
+        
+        Console.WriteLine($"Character Name: {name}");
+        Console.WriteLine($"Character Health: {health}");
+        Console.WriteLine($"Character Attack Power: {attackPower}");
+        Console.WriteLine($"Character Is Paralised: {isParalised}");
+        Console.WriteLine($"Charater Coin: {coin}");
+    }
+}
