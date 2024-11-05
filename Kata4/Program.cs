3e320dc4e896b5049975a383f359160584448ba8
Zätha Malmberg <zathamalmberg@gmail.com>
2024-11-05 14:19:18 +0100

    kata4 complete

---
+++ Kata4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Kata4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 2; i++)
+        {
+            Console.WriteLine($"Wave {i} is starting!");
+            for (int j = 1; j <= 4; j++)
+            {
+                Console.WriteLine($"   Enemy #{j} has spawned.");
+            }
+            Console.WriteLine($"Wave {i} completed. Preparing for hte next wave...");
+            Console.WriteLine();
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("All waves completed! Victory is yours!");
+    }
+}
