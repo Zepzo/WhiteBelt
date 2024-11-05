80503e894402e8b3bb5db2a84fd97f491a269129
Zätha Malmberg <zathamalmberg@gmail.com>
2024-11-05 14:18:38 +0100

    miniKata4 complete

---
+++ MiniKata4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MiniKata4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Spawning enemies using a for loop:");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Enemy #{i} spawned!");
+        }
+        
+        Console.WriteLine();
+        
+        Console.WriteLine("Spawning enemies using a while loop:");
+
+        int enemyCounter = 1;
+        int maxEnemys = 4;
+        
+        while (enemyCounter < maxEnemys)
+        {
+            Console.WriteLine($"Enemy #{enemyCounter} spawned!");
+            enemyCounter++;
+        }
+    }
+}
